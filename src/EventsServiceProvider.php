23d1612b03de3020d8c4da80bbd71c174e59ab6a
lamia <lamia.zouaghi@medianet.com.tn>
2019-10-30 16:56:59 +0100

    Fixing publish problems

--- src/EventsServiceProvider.php
+++ src/EventsServiceProvider.php
@@ -11,44 +11,26 @@ class EventsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'events');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'events');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
         if ($this->app->runningInConsole()) {
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/events'),
-            ], 'assets');*/
 
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/events'),
-            ], 'lang');*/
+            // Publishing the views.
+            $this->publishes([
+                __DIR__ . '/../resources/views' => resource_path('views/vendor/events'),
+            ], 'views');
 
-            // Registering package commands.
-            // $this->commands([]);
+            // Publishing the config.
+            $this->publishes([
+                __DIR__ . '/../config/cms-events.php' => config_path('cms-events.php'),
+            ], 'config');
+
+            // Publishing migrations 
+            $this->publishes([
+                __DIR__ . '/../database/migrations/create_event_categories_table.stub.php' => database_path('migrations/cms/' . date('Y_m_d_His', time()) . '_create_event_categories_table.php'),
+                __DIR__ . '/../database/migrations/create_events_table.stub.php' => database_path('migrations/cms/' . date('Y_m_d_His', time()) . '_create_events_table.php'),
+            ], 'migrations');
+            
+            
         }
-
-        // Publishing the views.
-        $this->publishes([
-            __DIR__ . '/../resources/views' => resource_path('views/vendor/events'),
-        ], 'views');
-
-        // Publishing the config.
-        $this->publishes([
-            __DIR__ . '/../config/cms-events.php' => config_path('config/cms-events.php'),
-        ], 'config');
-
-        // Publishing migrations 
-        $this->publishes([
-            __DIR__ . '/../database/migrations/create_event_categories_table.stub.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_event_categories_table.php'),
-            __DIR__ . '/../database/migrations/create_events_table.stub.php' => database_path('migrations/' . date('Y_m_d_His', time()) . 'create_events_table.php'),
-        ], 'migrations');
     }
 
     /**
